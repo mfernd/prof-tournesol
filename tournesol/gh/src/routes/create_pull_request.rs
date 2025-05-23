8dbb87c54840d74383e473203879580937768233
Matéo Fernandez <mateo.fernandez73@gmail.com>
2025-05-23 13:19:11 +0200

    feat: add files field in create_pr_handler

--- tournesol/gh/src/routes/create_pull_request.rs
+++ tournesol/gh/src/routes/create_pull_request.rs
@@ -11,9 +11,32 @@ use tracing::{error, info};
 pub struct CreatePullRequestBody {
     owner: String,
     repo: String,
-    new_branch: String,
-    pr_title: String,
-    pr_body: String,
+    pr: PullRequest,
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PullRequest {
+    title: String,
+    body: String,
+    files: Vec<File>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct File {
+    /// Action to perform on the file (update/create)
+    action: FileAction,
+    /// Path to the file in the repository
+    path: String,
+    /// Content of the file
+    content: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub enum FileAction {
+    Create,
+    Update,
 }
 
 pub async fn create_pull_request_handler(
@@ -23,6 +46,7 @@ pub async fn create_pull_request_handler(
 ) -> Result<String, StatusCode> {
     let owner = req.owner;
     let repo_name = req.repo;
+    let new_branch_name = format!("fix/prof-tournesol/{}", uuid::Uuid::now_v7());
 
     let octocrab = crate::utils::get_octocrab_client_for_repo(
         state.github_app_id,
@@ -61,7 +85,7 @@ pub async fn create_pull_request_handler(
 
     // Create the new branch
     let new_branch = repo_handler
-        .create_ref(&Reference::Branch(req.new_branch.clone()), base_sha)
+        .create_ref(&Reference::Branch(new_branch_name.clone()), base_sha)
         .await
         .map_err(|e| {
             error!("create branch error: {:?}", e);
@@ -70,25 +94,55 @@ pub async fn create_pull_request_handler(
     info!(new_branch = ?new_branch.url.path(), "branch created");
 
     // Create changes
-    repo_handler
-        .create_file(NEW_FILE_PATH, COMMIT_MESSAGE, NEW_FILE_CONTENT)
-        .branch(&req.new_branch)
-        .author(CommitAuthor {
-            name: crate::utils::GITHUB_APP_NAME.into(),
-            email: crate::utils::GITHUB_APP_EMAIL.into(),
-            date: None,
-        })
-        .send()
-        .await
-        .map_err(|e| {
-            error!("create file error: {:?}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
+    for file in req.pr.files {
+        let new_file = match file.action {
+            FileAction::Create => {
+                let commit_message = format!("fix: created file {}", file.path);
+                repo_handler.create_file(file.path, commit_message, file.content)
+            }
+            FileAction::Update => {
+                let commit_message = format!("fix: updated file {}", file.path);
+                let content_items = repo_handler
+                    .get_content()
+                    .path(&file.path)
+                    .send()
+                    .await
+                    .map_err(|e| {
+                        error!("get file content error: {:?}", e);
+                        StatusCode::BAD_REQUEST
+                    })?
+                    .items;
+                let sha = content_items
+                    .first()
+                    .ok_or_else(|| {
+                        error!("file \"{}\" not found", file.path);
+                        StatusCode::BAD_REQUEST
+                    })?
+                    .sha
+                    .clone();
+                repo_handler.update_file(file.path, commit_message, file.content, sha)
+            }
+        };
+
+        new_file
+            .branch(&new_branch_name)
+            .author(CommitAuthor {
+                name: crate::utils::GITHUB_APP_NAME.into(),
+                email: crate::utils::GITHUB_APP_EMAIL.into(),
+                date: None,
+            })
+            .send()
+            .await
+            .map_err(|e| {
+                error!("create file error: {:?}", e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+    }
     info!(new_branch = ?new_branch.url.path(), "changes applied");
 
     let pr_created = pr_handler
-        .create(&req.pr_title, &req.new_branch, default_branch)
-        .body(&req.pr_body)
+        .create(&req.pr.title, &new_branch_name, default_branch)
+        .body(&req.pr.body)
         .maintainer_can_modify(true)
         .send()
         .await
@@ -101,7 +155,3 @@ pub async fn create_pull_request_handler(
 
     Ok("pull request created".into())
 }
-
-const COMMIT_MESSAGE: &str = "test message";
-const NEW_FILE_PATH: &str = "test.txt";
-const NEW_FILE_CONTENT: &str = "test content";
